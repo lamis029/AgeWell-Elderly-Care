223cdcd60b6783a0b37f0b455d25788139d4f92d
lamis029 <s140227@student.squ.edu.om>
2024-12-02 18:48:03 +0400

    2

--- angular-integration/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { MemoryTipsComponent } from './app/memory-tips/memory-tips.component';
-
-bootstrapApplication(AppComponent)
-.catch(err => console.error(err));
